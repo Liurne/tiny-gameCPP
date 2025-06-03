e0acd8eaadbeafe231eabee50fb7fba74e33b34e
Liurne <jules.coquard@gmail.com>
2025-06-03 17:51:23 +0200

    corrected isActionActive

--- srcs/class/Keyboard.cpp
+++ srcs/class/Keyboard.cpp
@@ -2,12 +2,12 @@
 
 Keyboard::Keyboard() {
     _actions[0] = (t_key){KEY_QUIT, MLX_KEY_ESCAPE, MLX_RELEASE};
-    _actions[1] = (t_key){KEY_SCREENSHOT, MLX_KEY_S, MLX_RELEASE};
+    _actions[1] = (t_key){KEY_SCREENSHOT, MLX_KEY_Q, MLX_RELEASE};
     _actions[2] = (t_key){KEY_GENERATE_MAP, MLX_KEY_G, MLX_RELEASE};
-    _actions[3] = (t_key){KEY_UP, MLX_KEY_UP, MLX_RELEASE};
-    _actions[4] = (t_key){KEY_DOWN, MLX_KEY_DOWN, MLX_RELEASE};
-    _actions[5] = (t_key){KEY_LEFT, MLX_KEY_LEFT, MLX_RELEASE};
-    _actions[6] = (t_key){KEY_RIGHT, MLX_KEY_RIGHT, MLX_RELEASE};
+    _actions[3] = (t_key){KEY_UP, MLX_KEY_W, MLX_RELEASE};
+    _actions[4] = (t_key){KEY_DOWN, MLX_KEY_S, MLX_RELEASE};
+    _actions[5] = (t_key){KEY_LEFT, MLX_KEY_A, MLX_RELEASE};
+    _actions[6] = (t_key){KEY_RIGHT, MLX_KEY_D, MLX_RELEASE};
 }
 
 Keyboard::~Keyboard() {
@@ -16,15 +16,13 @@ Keyboard::~Keyboard() {
 
 void Keyboard::update(mlx_key_data_t keydata) {
     for (int i = 0; i < KEY_COUNT; ++i) {
-        if (_actions[i].key == keydata.key) {
+        if (_actions[i].key == keydata.key)
             _actions[i].key_action = keydata.action;
-        }
     }
 }
 
 bool Keyboard::isActionActive(t_action action) const {
-    if (action < 0 || KEY_COUNT >= 7) {
+    if (action < 0 || action >= KEY_COUNT)
         return false; // Invalid action
-    }
     return (_actions[action].key_action == MLX_PRESS || _actions[action].key_action == MLX_REPEAT);
 }
